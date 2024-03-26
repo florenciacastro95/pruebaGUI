4788ba50072d5ebfa6b46054952e64128f22547d
florenciacastro95 <fmcastro95@gmail.com>
2024-03-25 23:10:12 -0300

    Le puse action al boton limpiar

--- pruebaGui/src/pruebagui/Restador.java
+++ pruebaGui/src/pruebagui/Restador.java
@@ -28,36 +28,38 @@ public class Restador extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        jpContenedordeRes = new javax.swing.JPanel();
         blueLabel = new javax.swing.JLabel();
         jbRestar = new javax.swing.JButton();
         jbLimpiar = new javax.swing.JButton();
         jtNum1 = new javax.swing.JTextField();
         jtNum2 = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
+        jlRestadorTitulo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(0, 51, 51));
-        jPanel1.setBorder(javax.swing.BorderFactory.createCompoundBorder());
-        jPanel1.setForeground(new java.awt.Color(0, 255, 0));
+        jpContenedordeRes.setBackground(new java.awt.Color(0, 51, 51));
+        jpContenedordeRes.setBorder(javax.swing.BorderFactory.createCompoundBorder());
+        jpContenedordeRes.setForeground(new java.awt.Color(0, 255, 0));
 
+        blueLabel.setBackground(new java.awt.Color(204, 255, 204));
+        blueLabel.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
         blueLabel.setText("BlueLabel");
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(89, 89, 89)
-                .addComponent(blueLabel)
-                .addContainerGap(110, Short.MAX_VALUE))
+        javax.swing.GroupLayout jpContenedordeResLayout = new javax.swing.GroupLayout(jpContenedordeRes);
+        jpContenedordeRes.setLayout(jpContenedordeResLayout);
+        jpContenedordeResLayout.setHorizontalGroup(
+            jpContenedordeResLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jpContenedordeResLayout.createSequentialGroup()
+                .addContainerGap(92, Short.MAX_VALUE)
+                .addComponent(blueLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(82, 82, 82))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(14, 14, 14)
-                .addComponent(blueLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
+        jpContenedordeResLayout.setVerticalGroup(
+            jpContenedordeResLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jpContenedordeResLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(blueLabel)
                 .addContainerGap(8, Short.MAX_VALUE))
         );
 
@@ -81,8 +83,8 @@ public class Restador extends javax.swing.JFrame {
             }
         });
 
-        jLabel1.setFont(new java.awt.Font("DialogInput", 2, 48)); // NOI18N
-        jLabel1.setText("RESTADOR");
+        jlRestadorTitulo.setFont(new java.awt.Font("DialogInput", 2, 48)); // NOI18N
+        jlRestadorTitulo.setText("RESTADOR");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -91,28 +93,28 @@ public class Restador extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(110, 110, 110)
-                        .addComponent(jbRestar))
-                    .addGroup(layout.createSequentialGroup()
                         .addGap(95, 95, 95)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jpContenedordeRes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                    .addComponent(jtNum2, javax.swing.GroupLayout.DEFAULT_SIZE, 69, Short.MAX_VALUE)
+                                    .addComponent(jtNum1))
+                                .addGap(82, 82, 82)
+                                .addComponent(jbLimpiar))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(110, 110, 110)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGroup(layout.createSequentialGroup()
-                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                        .addComponent(jtNum2, javax.swing.GroupLayout.DEFAULT_SIZE, 69, Short.MAX_VALUE)
-                                        .addComponent(jtNum1))
-                                    .addGap(82, 82, 82)
-                                    .addComponent(jbLimpiar))))))
-                .addContainerGap(80, Short.MAX_VALUE))
+                            .addComponent(jlRestadorTitulo)
+                            .addComponent(jbRestar))))
+                .addContainerGap(66, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(17, 17, 17)
-                .addComponent(jLabel1)
-                .addGap(40, 40, 40)
+                .addGap(16, 16, 16)
+                .addComponent(jlRestadorTitulo)
+                .addGap(41, 41, 41)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jtNum2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -125,7 +127,7 @@ public class Restador extends javax.swing.JFrame {
                         .addGap(27, 27, 27)
                         .addComponent(jbLimpiar)
                         .addGap(58, 58, 58)))
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jpContenedordeRes, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(24, 24, 24))
         );
 
@@ -133,7 +135,9 @@ public class Restador extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jbLimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbLimpiarActionPerformed
-        // TODO add your handling code here:
+        jtNum1.setText("");
+        jtNum2.setText("");
+        blueLabel.setText("Resultado");
     }//GEN-LAST:event_jbLimpiarActionPerformed
 
     private void jtNum1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtNum1ActionPerformed
@@ -189,10 +193,10 @@ public class Restador extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel blueLabel;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JButton jbLimpiar;
     private javax.swing.JButton jbRestar;
+    private javax.swing.JLabel jlRestadorTitulo;
+    private javax.swing.JPanel jpContenedordeRes;
     private javax.swing.JTextField jtNum1;
     private javax.swing.JTextField jtNum2;
     // End of variables declaration//GEN-END:variables
